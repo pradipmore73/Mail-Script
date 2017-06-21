1b7d72b19e6c3d5f06bab38683e56eeeb0244068
Pradip More <pradipmore73@gmail.com>
2017-06-21 11:44:55 +0530

    Update chk_fs.sh

--- chk_fs.sh
+++ chk_fs.sh
@@ -3,7 +3,7 @@ count=`df -h | grep -v Filesystem |sed 's/\%//g' | awk '{if($5>=80)print}' | wc 
 echo $count
 if [ count != 0 ]
 then
-./mail.sh    // Path for the script which sends mail 
+./mail.sh | telnet 10.20.30.40 25   // Path for the script which sends mail  | ip or FQDN of SMTP Server and SMTP Port (default port is 25)
 else
 echo "No Match with the pattern for $(hostname) as on $(date)"
 fi
